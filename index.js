51c00526a538d6b08b54fc832de46b20df81972d
sarbia-offical <xm36447864@126.com>
2022-04-01 22:29:45 +0800

    修改文件

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const sayhi = require('./api/sayHi');
 const app = express();
 app.use(express.json({
-    extend: false
+    extended: false
 }))
-app.use('/api/product', sayhi);
+app.use('/api/sayhi', sayhi);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     `Server is running in port ${PORT}`
